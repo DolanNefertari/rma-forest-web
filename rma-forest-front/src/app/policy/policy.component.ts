a4012b2c7b705bec8eb1b37617836ad749cf575d
Dylan Ortega <dylan.ortega@inobaviz.com>
2026-08-19 19:47:15 -0400

    add policy

--- rma-forest-front/src/app/policy/policy.component.ts
+++ rma-forest-front/src/app/policy/policy.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
-  selector: 'app-policy.component',
-  imports: [CommonModule, MatIconModule],
-  templateUrl: './policy.component.html',
-  styleUrl: './policy.component.scss',
+selector: 'app-policy',
+standalone: true,
+imports: [
+CommonModule,
+FormsModule,
+MatIconModule,
+MatButtonToggleModule
+],
+templateUrl: './policy.component.html',
+styleUrl: './policy.component.scss',
 })
-export class PolicyComponent {}
+export class PolicyComponent {
+
+selectedPolicy: 'sig' | 'stop-work' = 'sig';
+
+}
